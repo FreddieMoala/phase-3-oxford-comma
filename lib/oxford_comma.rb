2b8f685aa5aaaaedf9eedcbfc1634fb43b42eaae
Freddie <loto_toa@hotmail.com>
2023-10-09 21:44:38 +1100

    Done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,22 @@
 def oxford_comma(array)
+    if array.length ==1
+        return array[0]
+
+end
+
+if array.length ==2
+    return "#{array[0]} and #{array[1]}"
+end
+
+if array.length ==3
+    return "#{array[0]}, #{array[1]}, and #{array[2]}"
+
+end
+
+if array.length >3
+    array[-1].insert(0, "and ")
+    return array.join(", ")
+
+end
 
 end
